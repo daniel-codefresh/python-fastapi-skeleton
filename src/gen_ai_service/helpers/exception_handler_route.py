9e356d1ac046382665552846d01e58910852b9d9
Daniel Soifer <daniel.soifer@codefresh.io>
2023-07-14 13:21:09 +0300

    fix exception handler route

---
+++ src/gen_ai_service/helpers/exception_handler_route.py
@@ -0,0 +1,37 @@
+from typing import Callable
+
+from fastapi import HTTPException
+from fastapi.exceptions import RequestValidationError
+from fastapi.routing import APIRoute
+from loguru import logger
+from starlette.requests import Request
+from starlette.responses import Response
+
+
+class ExceptionHandlerRoute(APIRoute):
+    """
+    Custom APIRoute that handles application errors and exceptions.
+    This is a wrapper for the original APIRoute class.
+    We use this instead of an exception handler, because exception handlers
+    for Exception class are buggy in FastAPI and mess up middleware execution order.
+    """
+
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
+
+        async def custom_route_handler(request: Request) -> Response:
+            try:
+                return await original_route_handler(request)
+            except Exception as exc:
+                if (
+                    isinstance(exc, HTTPException)
+                    or isinstance(exc, RequestValidationError)
+                    or isinstance(exc, ValueError)
+                ):
+                    raise exc
+                logger.exception(exc)
+                raise HTTPException(
+                    status_code=500, detail="An unexpected error occurred."
+                ) from exc
+
+        return custom_route_handler
