7f84ab5ac6b7939a77f0330c21011dbc18e0fc70
Daniel Soifer <daniel.soifer@codefresh.io>
2023-07-14 20:04:31 +0300

    .

--- src/gen_ai_service/api/cron/routes.py
+++ src/gen_ai_service/api/cron/routes.py
@@ -14,7 +14,8 @@ router = APIRouter(route_class=ExceptionHandlerRoute)
 async def generate_cron(
     cron_prompt: CronPrompt,
     cron_generator: Annotated[
-        CronExpressionGenerator, Depends(get_cron_expression_generator)
+        CronExpressionGenerator,
+        Depends(get_cron_expression_generator),
     ],
 ) -> dict:
     result = await cron_generator.generate_cron_expr(cron_prompt.text)
